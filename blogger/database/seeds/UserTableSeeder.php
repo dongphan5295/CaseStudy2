be2fffe519170ac6f197b8703dce3e8d314d5ed2
Your Name <you@example.com>
2020-02-28 14:09:32 +0700

    Update

--- blogger/database/seeds/UserTableSeeder.php
+++ blogger/database/seeds/UserTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserTableSeeder extends Seeder
 {
@@ -15,7 +16,7 @@ class UserTableSeeder extends Seeder
         DB::table('users')->insert([
             'name' => 'Dong Phan',
             'email' => 'dongdontcare@outlook.com',
-            'password' =>'dong2015',
+            'password' => Hash::make('dong2015'),
         ]);
     }
 }
